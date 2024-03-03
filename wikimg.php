cf4c60d0cb8dfcf45de8cd6609f29632da4af156
biglavis <lavis.chen5@gmail.com>
2024-03-03 14:46:39 -0800

    fixed bug on armors/classes with multiple tables

--- wikimg.php
+++ wikimg.php
@@ -52,10 +52,20 @@ function wikimg($page) {
 
         case "Classes":
         case "Armors":
-            if ($finder->query("//div[@class='yui-content']")->length > 0) {
-                $image0 = $finder->query("(//div[@id='wiki-tab-0-0'])[last()]//img")->item(0);
-                $image1 = $finder->query("(//div[@id='wiki-tab-0-1'])[last()]//img")->item(0);
-    
+            $image0 = $finder->query("(//div[@id='wiki-tab-0-0'])[last()]/img")->item(0);
+            if ($image0) {
+                $image1 = $finder->query("(//div[@id='wiki-tab-0-1'])[last()]/img")->item(0);
+
+                $image0->setAttribute("height", "65%");
+                $image1->setAttribute("height", "65%");
+
+                return array($doc->saveHTML($image0), $doc->saveHTML($image1));
+            }
+
+            $image0 = $finder->query("(//div[@id='wiki-tab-0-0'])[last()]//div[@class='collapsible-block-content']/img")->item(0);
+            if ($image0) {
+                $image1 = $finder->query("(//div[@id='wiki-tab-0-1'])[last()]//div[@class='collapsible-block-content']/img")->item(0);
+                    
                 $image0->setAttribute("height", "65%");
                 $image1->setAttribute("height", "65%");
 

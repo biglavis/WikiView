c76c5dc6725b835b1486a6f854dfdbb4d1514a0e
biglavis <lavis.chen5@gmail.com>
2024-03-02 20:50:54 -0800

    minor change

--- wikiview.js
+++ wikiview.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            WikiView: AQW Link Preview
 // @namespace       https://github.com/biglavis/
-// @version         1.0.2
+// @version         1.0.1
 // @description     Adds image previews for links on the official AQW Wiki, AQW character pages, and AQW account management.
 // @match           http://aqwwiki.wikidot.com/*
 // @match           https://account.aq.com/CharPage?id=*
